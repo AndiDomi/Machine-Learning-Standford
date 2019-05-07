d70579400d193019f4dae01824f04d45e7214fec
Andi Domi <1988.domi@gmail.com>
2019-05-07 18:31:41 +0300

    logistic regression cost and gradient done

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -20,6 +20,58 @@ grad = zeros(size(theta));
 
 
 
+function g = sigmoid(z)
+      g=1./(1+e.^(-z));
+end
+
+
+temp = 0;
+
+i = 1;
+
+O0 = theta;
+
+while i <= m,
+  h0 = sigmoid(X(i,:)*O0);
+  temp = temp + ( ( -y(i)'*log(h0) ) - ( (1-y(i))'*log(1-h0) ) );
+  i = i + 1;
+end;
+
+j=2;
+temp_reg = 0;
+
+while j<= size(theta)(1),
+    temp_reg = temp_reg + O0(j);
+    j = j+1;
+end
+
+J = (temp/m) + (temp_reg*lambda)/(2*m);
+
+temp_01 = 0;
+i = 1;
+j = 1;
+O0 = theta;
+
+
+   while j <= size(theta)(1),
+     while i <= m,
+       h0 = sigmoid(X(i,:)*O0);
+       temp_01 =  temp_01 + (( h0 - y(i))*X(i)(j));
+         i = i + 1;
+     end;
+      j = j + 1;
+   end;
+
+%grad = temp_01 / m;
+O0 = theta;
+O0(1)=0;
+grad = ( (1/m)*( X'*( sigmoid(X*theta)-y))) + (lambda/m)*O0;
+  
+% working one , but why is working?
+%grad = (1/m) * (X' * (sigmoid(X*theta)-y));
+
+
+
 
 
 % =============================================================
